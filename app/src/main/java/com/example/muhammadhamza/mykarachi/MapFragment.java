cdb8ba4dccbe8f62d293be5ddb935bb6f1703ed7
Muhammad Hamza <muhammadhamza20>
2018-07-30 02:14:52 +0500

    Second Commit

--- app/src/main/java/com/example/muhammadhamza/mykarachi/MapFragment.java
+++ app/src/main/java/com/example/muhammadhamza/mykarachi/MapFragment.java
@@ -28,6 +28,7 @@ import android.widget.ImageView;
 import android.widget.TextView;
 import android.widget.Toast;
 
+
 import com.google.android.gms.common.ConnectionResult;
 import com.google.android.gms.common.GoogleApiAvailability;
 import com.google.android.gms.common.GooglePlayServicesNotAvailableException;
